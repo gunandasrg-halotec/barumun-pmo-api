ffb20ecfa5897e1029d72657c67a78b72cc824f1
Ezra Dalimunthe <ezra.dalimunthe@gmail.com>
2026-06-15 18:33:19 +0700

    add columns last_login_at and phone

---
+++ database/migrations/2026_06_15_082320_alter_users_table001.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string("phone", length: 30)->nullable();
+            $table->timestamp("last_login_at")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn("phone");
+            $table->dropColumn("last_login_at");
+        });
+    }
+};
